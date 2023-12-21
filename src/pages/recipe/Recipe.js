7089692f516051600103e6e6a03e6c6e7a024738
wwwchendev <wwwchendev@gmail.com>
2023-12-21 21:54:04 +0800

    完成食譜詳細資料頁面

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,8 +1,32 @@
 import React from 'react'
 import './Recipe.css'
+import { useFetch } from "../../hooks/useFetch"
+import { useParams } from 'react-router-dom'
 
 export default function Recipe() {
+  const {id} = useParams()
+  const { data, isPending, error, postData } = useFetch(`http://localhost:3000/recipes/${id}`)
   return (
-    <p>Recipe</p>
+
+    <div className="recipe">
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && <>
+        <h2 className="page-title">{data.title}</h2>
+        <ul>
+          {
+            data.ingredients.map(ingredient=><li>{ingredient}</li>)
+          }
+        </ul>
+        <div className="method">{data.method}</div>
+      </>
+      }
+      {/* <div className="">
+        {
+          data&& JSON.stringify(data)
+        }
+      </div> */}
+    </div>
+
   )
 }
