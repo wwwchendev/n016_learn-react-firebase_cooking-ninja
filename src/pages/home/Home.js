e2a0cbb0946f0401be755d3fcc98370c5986aa11
wwwchendev <wwwchendev@gmail.com>
2023-12-21 20:59:38 +0800

    使用useFetch進行請求並渲染data

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
+import { useFetch } from "../../hooks/useFetch"
 
 export default function Home() {
+  const {data, isPending, error, postData} = useFetch(`http://localhost:3000/recipes`)
   return (
-    <p>Home</p>
+    <div className="home">
+      {error && <p className='error'>{error}</p>}
+      {isPending && <p className='loading'>Loading...</p>}
+      {data && data.map((item,idx)=>{
+        return <h2>{item.title}</h2>
+      })
+      }
+    </div>
   )
 }
