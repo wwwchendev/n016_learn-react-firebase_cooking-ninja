52b9238925a5388ba874ee26cbb28a6cfff1cecf
wwwchendev <wwwchendev@gmail.com>
2023-12-22 23:18:50 +0800

    RecipeList.js完成移除食譜功能

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -1,14 +1,31 @@
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 import { useThemeContext } from '../hooks/useThemeContext'
 
+import { firestore } from '../firebase/config';
+import { doc, getDoc, deleteDoc } from 'firebase/firestore';
 // styles
 import './RecipeList.css'
+import Trashcan from '../assets/trashcan.svg'
 
-export default function RecipeList({ recipes }) {
+export default function RecipeList({ recipes, }) {
   const { mode } = useThemeContext()
 
   if(recipes.length===0){
     return <p className="error">查無相關食譜</p>
+  }
+
+  const handleClick = async(id)=>{
+    try{
+      const recipeRef = doc(firestore, 'recipes', id);
+      const snapshot = await getDoc(recipeRef);
+      const recipeData = snapshot.data();
+      await deleteDoc(recipeRef);
+      if (recipeData) {
+        alert(`已刪除食譜 ${recipeData.title}`);
+      }
+    }catch(error){
+        console.log(error);
+    }
   }
 
   return (
@@ -19,6 +36,11 @@ export default function RecipeList({ recipes }) {
           <p>上菜時間 {recipe.cookingTime}</p>
           <div>{recipe.method.substring(0, 100)}...</div>
           <Link to={`/recipes/${recipe.id}`}>查看食譜</Link>
+          <img 
+            className="delete"
+            onClick={() => handleClick(recipe.id)}
+            src={Trashcan} alt="delete icon" 
+          />
         </div>
       ))}
     </div>
