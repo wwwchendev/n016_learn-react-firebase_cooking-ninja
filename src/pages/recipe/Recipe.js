c7370bb4cd66c7a4f18d30f3c833848a64214696
wwwchendev <wwwchendev@gmail.com>
2023-12-22 21:56:16 +0800

    Recipe.js取得指定ID食譜並進行資料渲染

--- src/pages/recipe/Recipe.js
+++ src/pages/recipe/Recipe.js
@@ -1,13 +1,50 @@
-import React from 'react'
-import './Recipe.css'
-import { useFetch } from "../../hooks/useFetch"
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { useThemeContext } from '../../hooks/useThemeContext'
 
+import './Recipe.css'
+
+// import { useFetch } from "../../hooks/useFetch"
+import { firestore } from '../../firebase/config';
+import { doc, getDoc } from 'firebase/firestore';
+
+// TODO Firebase Firestore
+// doc()： 用於創建指向特定文檔（Document）的引用。
+// getDoc()： 這個方法用於獲取特定文檔的快照。
+// collection()： 用於獲取指向特定集合（Collection）的引用。
+// getDocs()： 這個方法用於獲取集合中所有文檔的快照。
+
 export default function Recipe() {
   const {id} = useParams()
-  const { data, isPending, error, postData } = useFetch(`http://localhost:3000/recipes/${id}`)
   const { mode } = useThemeContext()
+  // 定義資料請求狀態
+  // const { data, isPending, error, postData } = useFetch(`http://localhost:3000/recipes/${id}`)
+  const [ data , setData ] = useState(null)
+  const [ isPending, setIsPending ] = useState(false)
+  const [ error, setError ] = useState(false)
+  
+  // 宣告請求資料的函數
+  async function fetchData() {
+    try{
+      const recipeRef = doc(firestore, 'recipes', id);
+      const snapshot = await getDoc(recipeRef);
+      if (snapshot.exists()) {
+        setData(snapshot.data());        
+        setIsPending(false)
+      } else {
+        setError(`找不到食譜資料`) 
+        setIsPending(false)
+      }
+    }catch(error){
+      console.log(error);
+    }
+  }
+
+  // 發送請求取得firebase資料庫內容
+  useEffect(() => {
+    setIsPending(true) 
+    fetchData();
+  }, []);
 
   return (
 
