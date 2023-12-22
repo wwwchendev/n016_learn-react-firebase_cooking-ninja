e74dec3d55cd398a354d9b5ad2b07b18674e6814
wwwchendev <wwwchendev@gmail.com>
2023-12-22 13:04:00 +0800

    完成顏色選擇功能

--- src/components/ThemeSelector.js
+++ src/components/ThemeSelector.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import './ThemeSelector.css'
-
-const themeColors = ['#58249c', '#249c6b', '#b70233']
+import { useThemeContext } from '../hooks/useThemeContext'
 
 export default function ThemeSelector() {
+    const { setColor } = useThemeContext()
+    const themeColors = ['#58249c', '#249c6b', '#b70233']
 
     return (
         <div className="theme-selector">
             <div className="theme-buttons">
                 {themeColors.map(color => (
-                    <div key={color} style={{ background: color }} />
+                    <div 
+                    key={color} 
+                    style={{ background: color }} 
+                    onClick={()=>{setColor(color)}}
+                    />
                 ))}
             </div>
         </div>
