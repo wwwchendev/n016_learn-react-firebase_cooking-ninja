17cb93c5855471e85e17c849154eb6cf48741f03
wwwchendev <wwwchendev@gmail.com>
2023-12-22 08:43:56 +0800

    調整react-router相關代碼撰寫方式 提高可讀性

--- src/Router.js
+++ src/Router.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import { BrowserRouter,HashRouter,Routes,Route } from 'react-router-dom'
-import { Home,Create,Recipe,Search} from './pages'
+import { Routes, Route } from 'react-router-dom'
+import { Home, Create, Recipe, Search } from './pages'
 import Navbar from './components/Navbar'
+
 export default function Router() {
-  return (
-    <BrowserRouter>
-        <Navbar/>
-        <Routes>
-            <Route path="/" exact element={<Home/>}/>
-            <Route path="/create" element={<Create/>}/>
-            <Route path="/search" element={<Search/>}/>
-            <Route path="/recipes/:id" element={<Recipe/>}/>
-        </Routes>
-    </BrowserRouter>
+  return (<>
+    <Navbar />
+    <Routes>
+      <Route path="/" exact element={<Home />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/recipes/:id" element={<Recipe />} />
+    </Routes>
+  </>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Router from './Router';
@@ -6,7 +7,9 @@ import Router from './Router';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router />
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
